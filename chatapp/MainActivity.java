22c2de90af79829ffc1627458097b61530ce5f91
kasettilameri <riikos@gmail.com>
2017-10-10 00:08:04 +0300

    changes to structure

--- chatapp/MainActivity.java
+++ chatapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kasettilameri.chatapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button sendButton;
     private EditText editText;
-    private Receiver receiver;
+    private Messenger messenger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +23,18 @@ public class MainActivity extends AppCompatActivity {
         sendButton = (Button)findViewById(R.id.sendButton);
         editText = (EditText)findViewById(R.id.editText);
 
-        receiver = new Receiver(this);
-        Thread receiverThread = new Thread(receiver);
-        receiverThread.start();
+        messenger = new Messenger(this);
+        Thread messengerThread = new Thread(messenger);
+        messengerThread.start();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //gettxt,sendtxt,cleartxt
+                messenger.sendMsg(editText.getText().toString());
+                editText.getText().clear();
+            }
+        });
     }
 
     public void appendMsg(String msg) {
